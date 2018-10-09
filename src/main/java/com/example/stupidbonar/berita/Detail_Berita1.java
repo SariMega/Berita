32ca96a3fe66acf47cb063412f295341744da279
Megasari Wulan Sakti <43538655+SariMega@users.noreply.github.com>
2018-10-09 15:03:04 +0700

    Add files via upload

---
+++ src/main/java/com/example/stupidbonar/berita/Detail_Berita1.java
@@ -0,0 +1,13 @@
+package com.example.stupidbonar.berita;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail_Berita1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail__berita1);
+    }
+}
---
+++ src/main/java/com/example/stupidbonar/berita/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.stupidbonar.berita;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage1 (View view) {
+        Intent intent = new Intent(this, Detail_Berita_1.class);
+        startActivity(intent);
+    }
+
+    public void sendMessage2 (View view){
+        Intent intent = new Intent(this, Detail_Berita_2.class);
+        startActivity(intent);
+    }
+
+    public void sendMessage3 (View view){
+        Intent intent = new Intent(this, Detail_Berita_3.class);
+        startActivity(intent);
+    }
+
+}
